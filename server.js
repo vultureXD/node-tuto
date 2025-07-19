fccda574a2f39e2874b6687c0b51121bdee6d9d9
hasan <149801904+vultureXD@users.noreply.github.com>
2025-07-19 22:00:26 +0300

    ss

--- server.js
+++ server.js
@@ -1,15 +1,36 @@
 import http from "http";
+import fs from "fs/promises";
+import url from "url";
+import path from "path";
 
-const PORT = 8000;
+const PORT = process.env.PORT;
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-const server = http.createServer((req, res) => {
-    // res.setHeader("Content-Type", "text/html");
-    // res.statusCode = 404;\
+console.log(__filename, __dirname);
+const server = http.createServer(async (req, res) => {
+    try {
+        if (req.method === "GET") {
+            let filePath;
+            if (req.url === "/") {
+                filePath = path.join(__dirname, "public", "index.html");
+            } else if (req.url === "/about") {
+                filePath = path.join(__dirname, "public", "about.html");
+            } else {
+                throw new Error("Not found");
+            }
 
-    res.writeHead(500, { "Content-Type": "application/json" });
-    res.end(JSON.stringify({
-        message: "server Error"
-    }));
+            const data = await fs.readFile(filePath);
+            res.setHeader("Content-Type", "text/html");
+            res.write(data);
+            res.end();
+        } else {
+            throw new Error("Method not allowed");
+        }
+    } catch (error) {
+        res.writeHead(500, { "Content-Type": "text/html" });
+        res.end("<h1>Server Error</h1>");
+    }
 });
 
 server.listen(PORT, () => {
